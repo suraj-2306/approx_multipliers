690890f18497f793485cb25c6501e2c8e1e41a2e
raj <suraj.s2306@gmail.com>
2021-02-11 00:10:12 +0530

    Changed a few lines to optimize the run time

--- matlab_implementation/perforation_and_rounding/error_analysis.m
+++ matlab_implementation/perforation_and_rounding/error_analysis.m
@@ -1,11 +1,15 @@
 function [MRED] = error_analysis(A,B,m,k)
+	resolution = 1024;
+	tic
 	RED =0.000;
+	RED = double(RED);
 	%result = booth_multiplier_radix_2(rounding(A,m),perforation(B,k));
-	for j=-32768:32767
-		for i = -32768:32767
+	for j=-resolution:resolution-1
+		for i = -resolution:resolution-1
 			RED = RED+ abs(i*j - booth_multiplier_radix_2(rounding(i,m),perforation(j,k)))/abs(i*j)
 		end
 	end
 	MRED = double(RED/2^16);
+	timeElapsed = toc
 end
 	
